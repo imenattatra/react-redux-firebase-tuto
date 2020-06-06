1513193c256ba78a0bb0e37cc3fb5294213ace33
zolicoeur <ci_attatra@esi.dz>
2020-06-06 22:38:03 +0200

    add firestore reducer and async with firestore data

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React,{Component} from 'react';
 import Notifications from './Notifications';
 import ProjectList from '../projects/ProjectList';
 import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 class Dashboard extends Component {
 
     render() { 
@@ -22,9 +24,12 @@ class Dashboard extends Component {
 }
 
 const mapStateToProp = (state)=>{
-    console.log(state);
+    //console.log(state);
     return{
-        projects:state.project.projects
+       projects:state.firestore.ordered.projects
     }
 }
-export default connect(mapStateToProp)(Dashboard);
+export default compose(
+    connect(mapStateToProp),
+    firestoreConnect(['projects'])
+    )(Dashboard);
